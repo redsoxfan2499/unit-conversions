bf41ba3af209fe368d70d8ad450d40ec59f5aadf
Darren Ladner <darren.ladner@ridetm.org>
2020-10-01 12:45:53 -0500

    added Weight class, kilo to lbs, and UnitTest

--- src/UnitConversionsClass.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Hyperdrivedesigns\UnitConversions;
-
-class UnitConversionsClass
-{
-}
---
+++ src/Weight.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Hyperdrivedesigns\UnitConversions;
+
+class Weight
+{
+    private $kilograms;
+
+    public function __construct(float $kilograms)
+    {
+        $this->kilograms = $kilograms;
+    }
+
+    public static function fromKilograms(float $kilograms): self
+    {
+        return new static($kilograms);
+    }
+
+    public function toLbs(): float
+    {
+        return $this->kilograms * 2.204623;
+    }
+
+
+}
